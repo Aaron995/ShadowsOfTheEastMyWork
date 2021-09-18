3534780e625cdde5cba0f1b9ed837b2de4a60d11
Aaron995 <46718776+Aaron995@users.noreply.github.com>
2021-09-18 11:06:26 +0200

    Added interface I forgot to add

---
+++ Scripts/IDamagable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamagable 
+{
+    /// <summary>
+    /// Inflects damage to the parent of this interface.
+    /// </summary>
+    /// <param name="amount">The amount of damage being dealt.</param>
+    public void TakeDamage(int amount);
+}
